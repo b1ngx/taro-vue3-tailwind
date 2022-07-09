95f254b54774f0d4a05a67dba8d86e73c5209d2a
Heart <b1ngx@qq.com>
2022-07-09 13:59:06 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="flex p-2.5">
+    <view class="p-2.5 w-1/2">
+      <text>1</text>
+    </view>
+    <view class="p-2.5 w-1/2">
+      <image src="../../cover.jpg" />
+    </view>
+    <view class="p-2.5 w-1/2">
+      <image src="../../cover.jpg" />
+    </view>
+    <view class="p-2.5 w-1/2"
+      >ets
+      <image src="../../cover.jpg" />
+    </view>
+  </view>
+</template>
