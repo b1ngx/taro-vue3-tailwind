d5c9a77cb2bfeb342bdc2f1166b79c490d3961ad
Heart <b1ngx@qq.com>
2022-07-22 14:49:20 +0800

    feat: build

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,14 +3,20 @@
     <view class="p-2.5 w-1/2">
       <text>1</text>
     </view>
-    <view class="p-2.5 w-1/2">
+    <view class="p-2.5 w-1/2" v-for="item in list" :key="item">
+      <image src="../../cover.jpg" />
+    </view>
+    <!-- <view class="p-2.5 w-1/2">
       <image src="../../cover.jpg" />
     </view>
     <view class="p-2.5 w-1/2">
       <image src="../../cover.jpg" />
-    </view>
-    <view class="p-2.5 w-1/2">
-      <image src="../../cover.jpg" />
-    </view>
+    </view> -->
   </view>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const list = ref([1, 2, 3]);
+</script>
